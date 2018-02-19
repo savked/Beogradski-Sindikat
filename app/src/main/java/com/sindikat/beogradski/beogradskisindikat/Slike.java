c2e9f8be950fcad1c281ed90319bfcb7f3406d22
savked <savadimitrijevic99@gmail.com>
2018-02-19 14:18:32 +0100

    Able to change wallpapers by clicking on the pictures.

--- app/src/main/java/com/sindikat/beogradski/beogradskisindikat/Slike.java
+++ app/src/main/java/com/sindikat/beogradski/beogradskisindikat/Slike.java
@@ -1,11 +1,14 @@
 package com.sindikat.beogradski.beogradskisindikat;
 
+import android.app.WallpaperManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
+
+import java.io.IOException;
 
 public class Slike extends AppCompatActivity {
     private GridView gridView;
@@ -21,7 +24,23 @@ public class Slike extends AppCompatActivity {
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent,
                                     View v, int position, long id) {
-                Toast.makeText(getBaseContext(), "Grid Item " + (position + 1) + " Selected", Toast.LENGTH_LONG).show();
+                WallpaperManager myWallpaperManager
+                        = WallpaperManager.getInstance(getApplicationContext());
+                try {
+                    if(position == 0)
+                        myWallpaperManager.setResource(R.drawable.slika1);
+                    else if(position == 1)
+                        myWallpaperManager.setResource(R.drawable.slika2);
+                    else if(position == 2)
+                        myWallpaperManager.setResource(R.drawable.slika3);
+                    else if(position == 3)
+                        myWallpaperManager.setResource(R.drawable.slika4);
+
+                    Toast.makeText(Slike.this, "Pozadina postavljena", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
             }
         });
     }
