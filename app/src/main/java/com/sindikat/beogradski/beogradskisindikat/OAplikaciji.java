538ae8b9ef370237a7aae90bc11d4b1bc07be023
savked <savadimitrijevic99@gmail.com>
2019-04-05 22:58:18 +0200

    Migrated to AndroidX, Updated Gradle

--- app/src/main/java/com/sindikat/beogradski/beogradskisindikat/OAplikaciji.java
+++ app/src/main/java/com/sindikat/beogradski/beogradskisindikat/OAplikaciji.java
@@ -1,11 +1,9 @@
 package com.sindikat.beogradski.beogradskisindikat;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class OAplikaciji extends AppCompatActivity {
 
--- app/src/main/java/com/sindikat/beogradski/beogradskisindikat/Slike.java
+++ app/src/main/java/com/sindikat/beogradski/beogradskisindikat/Slike.java
@@ -2,7 +2,7 @@ package com.sindikat.beogradski.beogradskisindikat;
 
 import android.app.WallpaperManager;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
