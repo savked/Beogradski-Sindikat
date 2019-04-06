4dd314d618c9579c211c414dcf14e0e6c5bba7b9
savked <savadimitrijevic99@gmail.com>
2019-04-06 21:32:27 +0200

    Significantly decreased app size, Added Picasso lib

---
+++ app/src/main/java/com/sindikat/beogradski/beogradskisindikat/utils/Constants.java
@@ -0,0 +1,5 @@
+package com.sindikat.beogradski.beogradskisindikat.utils;
+
+public class Constants {
+    public static final String URL_RESOURCES = "gs://beogradski-sindikat.appspot.com/Resources/";
+}
